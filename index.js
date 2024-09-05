1d8a11e623d1e92715d82f0444881e0d3599c56c
fahima bushra <bushra.arifeen@gmail.com>
2024-09-05 18:04:23 +0600

    eaton food server ready

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = process.env.DB_URL;
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,6 +23,86 @@ async function run() {
   try {
   
     await client.connect();
+    const foodCollection = client.db("foodDB").collection("foodCollection");
+    const userCollection = client.db("userDB").collection("userCollection");
+
+
+    app.post('/food', async (req,res)=>{
+      const foodData = req.body;
+      const result = await foodCollection.insertOne(foodData);
+      res.send(result)
+  })
+
+  app.get('/food', async (req,res)=>{
+    const foodData = foodCollection.find();
+    const result = await foodData.toArray();
+    res.send(result);
+  })
+
+  app.get('/food/:id', async (req,res)=>{
+    const id = req.params.id;
+    const foodData =  await foodCollection.findOne({_id:new ObjectId(id)});
+    res.send(foodData);
+  })
+
+  app.patch('/food/:id', async (req,res)=>{
+    const id = req.params.id;
+    const updatedData = req.body;
+    const result = await foodCollection.updateOne({_id:new ObjectId(id)},
+    {$set: updatedData}
+  );
+    res.send(result);
+  })
+
+  app.delete('/food/:id',async (req,res)=>{
+    const id = req.params.id;
+    const result = foodCollection.deleteOne({_id:new ObjectId(id)});
+    res.send(result);
+  })
+
+ app.post('/user', async(req,res)=>{
+  const user = req.body;
+  const token = createToken(user);
+  console.log(token)
+  const isUserExist = await userCollection.findOne({email: user?.email})
+  console.log(isUserExist)
+  if(isUserExist?._id){
+    return res.send({
+      status:"success",
+      message:"login success",
+      token
+    })
+  }
+  await userCollection.insertOne(user);
+  res.send(token)
+ })
+
+
+ app.get('/user', async (req,res)=>{
+  const userData = userCollection.find();
+  const result = await userData.toArray();
+  res.send(result);
+})
+
+
+ app.get('/user/get/:id', async (req,res)=>{
+  const id = req.params.id;
+  const result = await userCollection.findOne({_id:new ObjectId(id)});
+  res.send(result);
+ })
+
+ app.get('/user/:email', async (req,res)=>{
+  const email = req.params.email;
+  const result = await userCollection.findOne({email});
+  res.send(result);
+ })
+
+ app.patch('/user/:email', async (req,res)=>{
+  const email = req.params.email;
+  const userData = req.body;
+  const result = await userCollection.updateOne({email},{$set: userData},{upsert:true});
+  res.send(result);
+ })
    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
